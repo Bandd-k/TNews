2280db1bc123c354502453a268888c9216c2e4bd
Bandd-k <banddk1@gmail.com>
2017-12-10 20:50:37 +0300

    small change of placeholder

--- TNews/PresentationLayer/PlaceHolderActivityView/PlaceholderActivity.swift
+++ TNews/PresentationLayer/PlaceHolderActivityView/PlaceholderActivity.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 @IBDesignable class PlaceholderActivity: UIView {
-    // Our custom view from the XIB file
-    var view: UIView!
+    private var view: UIView!
     @IBInspectable var isDownloading:Bool = false {
         didSet{
             if isDownloading == true {
@@ -29,45 +28,28 @@ import UIKit
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var textLabel: UILabel!
     override init(frame: CGRect) {
-        // 1. setup any properties here
-        
-        // 2. call super.init(frame:)
+
         super.init(frame: CGRect(x: frame.origin.x, y: frame.origin.y, width: 200.0, height: 150.0))
-        
-        // 3. Setup view from .xib file
         xibSetup()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        // 1. setup any properties here
-        
-        // 2. call super.init(coder:)
         super.init(coder: aDecoder)
-        
-        // 3. Setup view from .xib file
+
         xibSetup()
-        //        self.view = loadViewFromNib() as! CustomView
     }
     
     
     func xibSetup() {
         view = loadViewFromNib()
-        
-        // use bounds not frame or it'll be offset
         view.frame = bounds
-        
-        // Make the view stretch with containing view
         view.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
-        
-        // Adding custom subview on top of our view (over any custom drawing > see note below)
         addSubview(view)
     }
     
     func loadViewFromNib() -> UIView {
         let bundle = Bundle(for: type(of:self))
         let nib = UINib(nibName: "PlaceholderActivity", bundle: bundle)
-        
-        // Assumes UIView is top level and only object in CustomView.xib file
         let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
         return view
     }
